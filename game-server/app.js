f8de851436193ccaf10934257b78e306f885b3ab
lixiaobing <li.xiaobing@phanta.cc>
2018-12-29 18:08:04 +0800

    chat server

--- game-server/app.js
+++ game-server/app.js
@@ -1,28 +1,30 @@
 var pomelo = require('pomelo');
-
 /**
  * Init app for client.
  */
 var app = pomelo.createApp();
-app.set('name', 'HelloWorld');
+app.set('name', 'chatofpomelo-websocket');
 
 // app configuration
 app.configure('production|development', 'connector', function(){
-  app.set('connectorConfig',
-    {
-      connector : pomelo.connectors.sioconnector,
-      // 'websocket', 'polling-xhr', 'polling-jsonp', 'polling'
-      transports : ['websocket', 'polling'],
-      heartbeats : true,
-      closeTimeout : 60 * 1000,
-      heartbeatTimeout : 60 * 1000,
-      heartbeatInterval : 25 * 1000
-    });
+	app.set('connectorConfig',
+		{
+			connector : pomelo.connectors.hybridconnector,
+			heartbeat : 3,
+		});
+});
+
+app.configure('production|development', 'gate', function(){
+	app.set('connectorConfig',
+		{
+			connector : pomelo.connectors.hybridconnector,
+			useProtobuf : true
+		});
 });
 
 // start app
 app.start();
 
-process.on('uncaughtException', function (err) {
-  console.error(' Caught exception: ' + err.stack);
+process.on('uncaughtException', function(err) {
+	console.error(' Caught exception: ' + err.stack);
 });
